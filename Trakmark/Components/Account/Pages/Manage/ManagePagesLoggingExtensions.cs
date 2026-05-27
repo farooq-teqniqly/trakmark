80cefc8109d81bf6947c3c4dd725e63903ac0b96
farooq-teqniqly <farooq@teqniqly.com>
2026-05-27 15:04:26 -0700

    Format file.

--- Trakmark/Components/Account/Pages/Manage/ManagePagesLoggingExtensions.cs
+++ Trakmark/Components/Account/Pages/Manage/ManagePagesLoggingExtensions.cs
@@ -3,18 +3,33 @@ namespace Trakmark.Components.Account.Pages.Manage;
 /// <summary>Source-generated log messages for account management pages.</summary>
 internal static partial class ManagePagesLoggingExtensions
 {
-    [LoggerMessage(Level = LogLevel.Information, Message = "User with ID '{UserId}' has disabled 2fa.")]
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "User with ID '{UserId}' has disabled 2fa."
+    )]
     internal static partial void LogDisabled2fa(this ILogger logger, string userId);
 
-[LoggerMessage(Level = LogLevel.Information, Message = "User with ID '{UserId}' has enabled 2FA with an authenticator app.")]
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "User with ID '{UserId}' has enabled 2FA with an authenticator app."
+    )]
     internal static partial void LogEnabled2fa(this ILogger logger, string userId);
 
-    [LoggerMessage(Level = LogLevel.Information, Message = "User with ID '{UserId}' deleted themselves.")]
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "User with ID '{UserId}' deleted themselves."
+    )]
     internal static partial void LogUserDeletedSelf(this ILogger logger, string userId);
 
-    [LoggerMessage(Level = LogLevel.Information, Message = "User with ID '{UserId}' has generated new 2FA recovery codes.")]
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "User with ID '{UserId}' has generated new 2FA recovery codes."
+    )]
     internal static partial void LogGeneratedRecoveryCodes(this ILogger logger, string userId);
 
-    [LoggerMessage(Level = LogLevel.Information, Message = "User with ID '{UserId}' has reset their authentication app key.")]
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "User with ID '{UserId}' has reset their authentication app key."
+    )]
     internal static partial void LogResetAuthenticatorKey(this ILogger logger, string userId);
 }
